80d3fdbe01954a338553907d5f1b97a162345e4a
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2022-05-02 18:10:44 -0300

    Link del W3Scholl Y prueba del home y formularios

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>TP Pokedex</title>
+</head>
+<body>
+<header>
+    <div class="w3-container w3-teal"> <h1>Logo</h1>
+        <h1 >Pokedex</h1></div>
+    <form action="" method="post">
+        <!--<label for="name">Nombre</label>-->
+        <input type="text" id="name" name="user_name" placeholder="Nombre">
+        <!-- <label for="surname">Apellido</label>-->
+        <input type="text" id="surname" name="user_surname" placeholder="Apellido">
+        <button type="submit" name="ingresar" >ingresar</button>
+    </form>
+</header>
+<footer>
+
+</footer>
+</body>
+</html>
